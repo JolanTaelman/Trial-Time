10866e53a3fbe6532115f39f6815283fe5505c59
Jolan <taelman.jolan@hotmail.Com>
2019-10-25 02:50:52 +0200

    later commit to check if it works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TrialsComponent } from './trials/trials.component';
+import { TrialComponent } from './trial/trial.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'trials',
+  component: TrialComponent
+},
+  { path: '',
+  redirectTo: '/trial',
+  pathMatch: 'full'
+},
+  { path: '**', component: TrialsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
